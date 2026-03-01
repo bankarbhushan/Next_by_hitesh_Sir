ec12fb463cb6aa157943d57ed4ea6b14dc3777b0
bhushan bankar <bhushan.bankar@suavisinc.com>
2026-03-01 10:57:35 +0530

    fixed  user profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,22 +1,40 @@
 import React from "react";
+import Link from "next/link";
+export default async function UserProfile({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
 
-interface UserProfileProps {
-  params: {
-    id: string;
-  };
-}
-
-const UserProfile = ({ params }: UserProfileProps) => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-orange-500 to-red-600">
-      <h1 className="text-2xl md:text-3xl text-center text-white">
-        Hey{" "}
-        <span className="p-3 rounded-lg bg-white text-orange-600 font-bold shadow-md">
-          {params.id}
-        </span>
-      </h1>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#0f0c29] via-[#1a1a40] to-[#0f2027] px-4">
+      <div
+        className="items-center w-full max-w-md p-8 rounded-3xl 
+                      bg-white/5 backdrop-blur-xl 
+                      border border-white/10 
+                      shadow-[0_0_40px_rgba(139,92,246,0.4)]"
+      >
+        <h1 className="text-3xl font-semibold text-center text-white">
+          Welcome 👋
+        </h1>
+
+        {/* Avatar */}
+        <div className="flex justify-center mb-6">
+          <div className="w-24 h-24 rounded-full bg-gradient-to-tr from-orange-500 to-red-500 flex items-center justify-center text-3xl font-bold text-white shadow-lg">
+            {id.charAt(0).toUpperCase()}
+          </div>
+        </div>
+        <div className="mt-6 px-6 w-full text-center py-3 bg-gradient-to-r from-orange-500 to-red-500 rounded-xl font-bold">
+          {id}
+        </div>
+        <Link
+          className="text-blue-500 hover:text-blue-600 text-center block mt-6 font-bold"
+          href="/"
+        >
+          Go to Home
+        </Link>
+      </div>
     </div>
   );
-};
-
-export default UserProfile;
+}
